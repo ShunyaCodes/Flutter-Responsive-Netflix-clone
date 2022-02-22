b11f7c1ff6723420688957ccf427c865485b1495
ShunyaCodes <patilabhilash1046@gmail.com>
2022-02-21 20:53:30 -0800

    Added video for demo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,61 +1,97 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:netflix_ui_clone/cubits/cubits.dart';
 import 'package:netflix_ui_clone/data/data.dart';
 import 'package:netflix_ui_clone/widgets/widgets.dart';
 
-class NavScreen extends StatefulWidget {
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
   @override
-  _NavScreenState createState() => _NavScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _NavScreenState extends State<NavScreen> {
-  final List<Widget> _screens = [
-    HomeScreen(key: const PageStorageKey('homeScreen')),
-    const Scaffold(),
-    const Scaffold(),
-    const Scaffold(),
-    const Scaffold(),
-  ];
+class _HomeScreenState extends State<HomeScreen> {
+  ScrollController _scrollController;
 
-  final Map<String, IconData> _icons = const {
-    'Home': Icons.home,
-    'Search': Icons.search,
-    'Coming Soon': Icons.queue_play_next,
-    'Downloads': Icons.file_download,
-    'More': Icons.menu,
-  };
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        context.read<AppBarCubit>().setOffset(_scrollController.offset);
+      });
+    super.initState();
+  }
 
-  int _currentIndex = 0;
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: BlocProvider<AppBarCubit>(
-        create: (_) => AppBarCubit(),
-        child: _screens[_currentIndex],
+      extendBodyBehindAppBar: true,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.cast),
+        onPressed: () => print('Cast'),
       ),
-      bottomNavigationBar: !Responsive.isDesktop(context)
-          ? BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: Colors.black,
-              items: _icons
-                  .map((title, icon) => MapEntry(
-                      title,
-                      BottomNavigationBarItem(
-                        icon: Icon(icon, size: 30.0),
-                        title: Text(title),
-                      )))
-                  .values
-                  .toList(),
-              currentIndex: _currentIndex,
-              selectedItemColor: Colors.white,
-              selectedFontSize: 11.0,
-              unselectedItemColor: Colors.grey,
-              unselectedFontSize: 11.0,
-              onTap: (index) => setState(() => _currentIndex = index),
-            )
-          : null,
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: BlocBuilder<AppBarCubit, double>(
+          builder: (context, scrollOffset) {
+            return CustomAppBar(scrollOffset: scrollOffset);
+          },
+        ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        scrollDirection: Axis.vertical,
+        slivers: [
+          SliverToBoxAdapter(
+            child: ContentHeader(featuredContent: sintelContent),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: Previews(
+                key: PageStorageKey('previews'),
+                title: 'Previews',
+                contentList: previews,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: PageStorageKey('myList'),
+              title: 'My List',
+              contentList: myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: PageStorageKey('originals'),
+              title: 'Netflix Originals',
+              contentList: originals,
+              isOriginals: true,
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                key: PageStorageKey('trending'),
+                title: 'Trending',
+                contentList: trending,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
